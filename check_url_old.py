f6b103a5ca2adc96475d4f17278ef51ca56db6cc
ajeesh875 <ajeesh875@gmail.com>
2023-07-25 20:37:45 +0530

    Update check_url_old.py

--- check_url_old.py
+++ check_url_old.py
@@ -1,18 +1,9 @@
-import requests
-import time
+from office365.sharepoint.client_context import ClientContext
+from tests import test_site_url, settings
 
-def check_website_status(url, interval):
-    while True:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print(f"The website {url} is up and running.")
-            else:
-                print(f"The website {url} is down with a status code: {response.status_code}.")
-        except requests.ConnectionError:
-            print(f"The website {url} is unreachable.")
+ctx = ClientContext(test_site_url)
+ctx.with_user_credentials(settings.get('user_credentials', 'username'),
+                          settings.get('user_credentials', 'password'))
 
-        time.sleep(interval)
-
-# Example usage
-check_website_status("https://www.bbc.com/news", 10)  # Check every 60 seconds
+web = ctx.web.get().execute_query()
+print(web.url)
