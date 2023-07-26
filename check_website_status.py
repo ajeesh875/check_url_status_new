0ee989c914df7cc751f8a01e00f0aeaa89edd265
ajeesh875 <ajeesh875@gmail.com>
2023-07-26 15:58:00 +0530

    Update check_website_status.py

--- check_website_status.py
+++ check_website_status.py
@@ -1,40 +1,46 @@
-import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-import smtplib
-from email.mime.text import MIMEText
 
-def send_email(subject, body, sender_email, receiver_email, password):
-    message = MIMEText(body)
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
+def login_to_sharepoint(username, password):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    
+    # Replace 'your_sharepoint_login_url' with the actual login page URL
+    login_url = 'your_sharepoint_login_url'
+    driver.get(login_url)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+    # Find and fill in the username and password fields
+    username_field = driver.find_element_by_id('username_input_id')
+    password_field = driver.find_element_by_id('password_input_id')
 
-def check_website_status(url, interval, sender_email, receiver_email, password):
-    while True:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                message = f"The website {url} is up and running."
-                print(message)
-            else:
-                message = f"The website {url} is down with a status code: {response.status_code}."
-                print(message)
-        except requests.ConnectionError:
-            message = f"The website {url} is unreachable."
-            print(message)
+    username_field.send_keys(username)
+    password_field.send_keys(password)
 
-        send_email("Website Status", message, sender_email, receiver_email, password)
-        time.sleep(interval)
+    # Submit the login form
+    login_button = driver.find_element_by_id('login_button_id')
+    login_button.click()
 
-print("Enter website like  https://www.example.com")
-website_url = input("Enter the website URL: ")
-interval = int(input("Enter the check interval in seconds: "))
-sender_email = input("Enter your email address: ")
-receiver_email = input("Enter the recipient's email address: ")
-password = input("Enter your email password: ")
+    # Add a sleep to allow the page to load after login (you can use WebDriverWait for more robust waits)
+    time.sleep(5)
+    return driver
 
-check_website_status(website_url, interval, sender_email, receiver_email, password)
+def extract_links_from_sharepoint_page(driver):
+    # Replace 'your_sharepoint_page_url' with the URL of the page you want to scrape
+    page_url = 'your_sharepoint_page_url'
+    driver.get(page_url)
+
+    # Find all links on the page
+    links = driver.find_elements_by_tag_name('a')
+
+    # Extract and print the URLs
+    for link in links:
+        print(link.get_attribute('href'))
+
+    driver.quit()
+
+if __name__ == "__main__":
+    username = "your_username"
+    password = "your_password"
+
+    driver = login_to_sharepoint(username, password)
+    extract_links_from_sharepoint_page(driver)
